a17d8ad606ad09cd0fe302f6ecd311504a624417
Dmitry Zakharov <zakharov@ibm.com>
2026-04-20 17:31:44 +0100

    remove shebang
    Signed-off-by: Dmitry Zakharov <zakharov@ibm.com>

--- tests/sql-injection-attack-test.ts
+++ tests/sql-injection-attack-test.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env bun
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
 
